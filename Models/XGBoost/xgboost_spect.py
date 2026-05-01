f0589bc2dcccf553059a39db75af562b5b51ec2d
Will <blomerth.w@gmail.com>
2026-05-01 15:43:46 -0400

    Implemented 5-fold CV on ensemble methods

--- Models/XGBoost/xgboost_spect.py
+++ Models/XGBoost/xgboost_spect.py
@@ -11,19 +11,24 @@ import shap
 
 #####     DATA     #####
 # data import
-file_path = "breast-cancer-wisconsin.data"
-columns = ["SampleID", "ClumpThickness", "UniformityCellSize",
-           "UniformityCellShape", "MarginalAdhesion", "SingleEpithelialCellSize", "BareNuclei",
-           "BlandChromatin", "NormalNucleoli", "Mitoses", "Class"]
-bcw = pd.read_csv(file_path, header=None, na_values='?', names=columns)
+columns = ["Class", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11",
+           "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23"] 
+training_data = pd.read_csv("SPECT.train", header=None, na_values='?', names=columns)
+test_data = pd.read_csv("SPECT.test", header=None, na_values='?', names=columns)
 
-# separate features from classes
-bcw['Class'] = bcw['Class'].replace({2: 0, 4: 1})  # 2 is benign, 4 is malignant
-X = bcw.drop(columns=["Class", "SampleID"])
-y = bcw["Class"]
+# training data
+X_train = training_data.drop(columns=["Class"])
+y_train = training_data["Class"]
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# test data
+X_test = test_data.drop(columns=["Class"])
+y_test = test_data["Class"]
+
+### split data based on representative numbers
+data = pd.concat([training_data, test_data])
+X = data.drop(columns=["Class"])
+y = data["Class"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 count_negative = (y_train == 0).sum()
 count_positive = (y_train == 1).sum()
 scale_weight = count_negative / count_positive
@@ -74,7 +79,7 @@ print(cm_test)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm_test,
                               display_labels=model.classes_)
 disp.plot()
-plt.savefig('confusion_mat_XGBbcw.png', bbox_inches='tight')
+plt.savefig('confusion_mat_XGBspect.png', bbox_inches='tight')
 plt.close()
 
 #calculate accuracy
@@ -106,7 +111,7 @@ shap_values = explainer(X_test)
 
 # summary plot
 shap.summary_plot(shap_values, X_test, show=False)
-plt.savefig('XGBbcw_shap_summary.png', bbox_inches='tight')
+plt.savefig('XGBspect_shap_summary.png', bbox_inches='tight')
 plt.close()
 
 # bar plot of mean SHAP values
